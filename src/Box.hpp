5887ab59968e174769450f8f983041e315257605
Alejandro Ortega Cadahia <85643095+alex-ortega-07@users.noreply.github.com>
2022-05-29 21:12:42 +0200

    Update Box.hpp

--- src/Box.hpp
+++ src/Box.hpp
@@ -6,14 +6,11 @@
 
 class Box{
     public:
-        // PONER AQUI VARIABLES COMO G Y H Y F, HACER QUE NO HAYA STRUCT NODE
-
         Box();
 
         void appear(SDL_Renderer *renderer, int size);
         void disappear(SDL_Renderer *renderer, int size);
 
-        // void set_color(SDL_Color box_color);
         void draw(SDL_Renderer *renderer, int x = 0, int y = 0, int w = 0, int h = 0, bool alt_vals = false);
 
         int width, height;
